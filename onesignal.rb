6da81b9ae324aee7a7e30efa5be14a6f5bf20b6f
Mirzalazuardi Hermawan <mirzalazuardi@users.noreply.github.com>
2023-10-13 14:57:51 +0700

    add methods to check validity

--- onesignal.rb
+++ onesignal.rb
@@ -26,37 +26,76 @@ class Onesignal
     )
   end
 
-  def response_body_hash(response_hash)
+  def self.response_body_hash(response_hash)
     JSON.parse(response_hash[:body])
+  end
+
+  def is_app_id_valid?
+    generate_random_user
+    create_user(@random_user)
+    result = !(view_user(@random_user)[:status] == 400)
+    delete_user(@random_user)
+    result
+  end
+
+  def is_rest_api_key_valid?
+    generate_random_user
+    result = create_user(@random_user)[:status] == 200
+    delete_user(@random_user)
+    result
+  end
+
+  def self.category_hash
+    {
+      os: %w[WindowsPush MacOSPush],
+      web: %w[ChromeExtensionPush ChromePush SafariPush FirefoxPush SafaryLegacyPush],
+      mobile: %w[iOSPush AndroidPush HuaweiPush FireOSPush],
+      other: %w[Email SMS]
+    }
   end
 
   def send_push_notif(channel: 'push', **params)
     response = conn.post('/api/v1/notifications') do |req|
       req.body = send("#{__method__}_params", channel: channel, **params).to_json
     end
-    response.to_hash
+    present_response(response)
   end
 
-  def create_user(alias_label: 'external_id', **params)
+  def create_user(alias_id, alias_label: 'external_id', **params)
     response = conn.post("/api/v1/apps/#{app_id}/users") do |req|
-      req.body = send("#{__method__}_params", alias_label: alias_label, language: 'en', **params).to_json
+      req.body = send("#{__method__}_params", alias_id, alias_label: alias_label, language: 'en', **params).to_json
     end
-    response.to_hash
+    present_response(response)
   end
 
   def view_user(alias_id, alias_label: 'external_id')
     response = conn.get("/api/v1/apps/#{app_id}/users/by/#{alias_label}/#{alias_id}")
-    response.to_hash
+    present_response(response)
+  end
+
+  def delete_user(alias_id, alias_label: 'external_id')
+    response = conn.delete("/api/v1/apps/#{app_id}/users/by/#{alias_label}/#{alias_id}")
+    present_response(response)
   end
 
   def create_subscription(type, token, alias_id, alias_label: 'external_id')
     response = conn.post("/api/v1/apps/#{app_id}/users/by/#{alias_label}/#{alias_id}/subscriptions") do |req|
       req.body = send("#{__method__}_params", type, token).to_json
     end
-    response.to_hash
+    present_response(response)
   end
 
   private
+
+  def generate_random_user
+    @random_user ||= SecureRandom.uuid
+  end
+
+  def present_response(response)
+    r = response.to_hash
+    r[:body] = self.class.response_body_hash(r)
+    r.with_indifferent_access
+  end
 
   def credential_hash
     {
@@ -92,34 +131,26 @@ class Onesignal
     to.instance_of?(String) || to.empty?
   end
 
-  def create_user_params(alias_label: 'external_id', language: 'en', **params)
-    params = send("#{__method__}_validation", params)
+  # alias_id, alias_label: alias_label, language: 'en', **params
+  def create_user_params(alias_id, alias_label: 'external_id', language: 'en', **params)
     result = {
       properties: {
         language: language
       },
-      identity: { alias_label.to_sym => params[:alias_id] }
+      identity: { alias_label.to_sym => alias_id }
     }
-    result['properties']['tags'] = params[:tags] unless params[:tags].empty?
-    result['subscriptions'] = params[:subscriptions] unless params[:subscriptions].empty?
+    result['properties']['tags'] = params[:tags] unless params[:tags].nil? || params[:tags].empty?
+    result['subscriptions'] = params[:subscriptions] unless params[:subscriptions].nil? || params[:subscriptions].empty?
     result
-  end
-
-  def create_user_params_validation(params)
-    params[:tags] ||= {}
-    params[:subscriptions] ||= []
-    params = params.with_indifferent_access
-    raise 'alias id should be exist' if params[:alias_id].nil?
-
-    params
   end
 
   # type options:
   # Email SMS iOSPush AndroidPush HuaweiPush FireOSPush
   # WindowsPush MacOSPush ChromeExtensionPush ChromePush
   # SafaryLegacyPush FirefoxPush SafariPush
-  def create_subscription_params(type, token)
-    {
+  def create_subscription_params(type, token, **params)
+    params = params.with_indifferent_access
+    res = {
       subscription: {
         type: type,
         token: token,
@@ -127,7 +158,15 @@ class Onesignal
         session_time: 60,
         session_count: 1
       }
-    }
+    }.with_indifferent_access
+    append_optional_subscription_params(res, **params)
+  end
+
+  def append_optional_subscription_params(prev, **params)
+    params.each do |k, v|
+      prev[:subscription][k] = v
+    end.with_indifferent_access
+    prev
   end
 end
 
